ace49980a7c3d62fe27e13b9d73d79d3af6eca83
Jur <jeroen@the1stfloor.net>
2024-05-11 18:33:37 +0200

    DebugDataSender fixed

--- Assets/com.edia.eye/Runtime/Scripts/DebugDataSender.cs
+++ Assets/com.edia.eye/Runtime/Scripts/DebugDataSender.cs
@@ -11,28 +11,28 @@ namespace Edia.Eye
     /// </summary>
     public class DebugDataSender : MonoBehaviour
     {
-
-        public bool isStarted = false;
-
+        public bool IsStarted = false;
         public static ILslTimer LslTimer;
         public bool UseLslTiming = true;
+        double timestampLsl;
+        EyeDataPackage ed;
+        float randomNewValue = 0.3f;
 
         public void StartAddingDummyEyedata()
         {
-            isStarted = true;
+            IsStarted = true;
         }
 
-        // Sends random data to the eDIA `EyeDataHandler` with 90Hz. 
+        // Sends random data to the eDIA `EyeDataHandler` 
         void Update()
         {
-            if (!isStarted)
+            if (!IsStarted)
                 return;
 
-            double timestampLsl;
-            timestampLsl = LslTimer != null ? LslTimer.GetTime() : 0;
 
-            EyeDataPackage ed = new EyeDataPackage();
-            ed.eye = "combined";
+            ed = new();
+
+            ed.eye = Edia.Constants.EyeId.CENTER.ToString().ToLower();
             ed.direction_x_local = UnityEngine.Random.Range(-0.2f, 0.2f);
             ed.direction_y_local = UnityEngine.Random.Range(-0.2f, 0.2f);
             ed.direction_z_local = UnityEngine.Random.Range(0.8f, 1f);
@@ -44,73 +44,11 @@ namespace Edia.Eye
             ed.rotation_y_local = UnityEngine.Random.Range(-60f, 60f);
             ed.rotation_z_local = 0f;
             ed.timestamp_et = 123f;
+
+            timestampLsl = LslTimer != null ? LslTimer.GetTime() : 0;
             ed.timestamp_lsl = 0f;
 
-            //        direction_x_local = 0f,
-            //        direction_y_local = 0f,
-            //        direction_z_local = 0f,
-            //        position_x_local = 0f,
-            //        position_y_local = 0f,
-            //        position_z_local = 0f,
-            //        diameter = 0f,
-            //        rotation_x_local = 0f,
-            //        rotation_y_local = 0f,
-            //        rotation_z_local = 0f,
-            //        openness = 0f,
-            //        timestamp_et = DateTime.UtcNow.Second,
-            //        timestamp_lsl = timestampLsl
-
-            //EyeDataHandler.Instance.AddEyeDataPackage(ed);
-
-            //    foreach (string eye in new string[] { "left", "right", "center" })
-            //    {
-
-            //        SingleEyeData tmpData;
-            //        switch (eye)
-            //        {
-            //            //case "left":
-            //            //    tmpData = eyeData.verbose_data.left;
-            //            //    break;
-            //            //case "right":
-            //            //    tmpData = eyeData.verbose_data.right;
-            //            //    break;
-            //            case "center":
-            //                tmpData = eyeData.verbose_data.combined.eye_data;
-            //                break;
-            //            default:
-            //                tmpData = eyeData.verbose_data.combined.eye_data;
-            //                break;
-            //        }
-
-            //        double timestampLsl;
-
-            //    if (LslTimer != null)
-            //    {
-            //        timestampLsl = LslTimer.GetTime();
-            //    }
-            //    else
-            //    {
-            //        timestampLsl = 0;
-            //    };
-
-            //    ed = new()
-            //    {
-            //        eye = eye,
-            //        direction_x_local = 0f,
-            //        direction_y_local = 0f,
-            //        direction_z_local = 0f,
-            //        position_x_local = 0f,
-            //        position_y_local = 0f,
-            //        position_z_local = 0f,
-            //        diameter = 0f,
-            //        rotation_x_local = 0f,
-            //        rotation_y_local = 0f,
-            //        rotation_z_local = 0f,
-            //        openness = 0f,
-            //        timestamp_et = DateTime.UtcNow.Second,
-            //        timestamp_lsl = timestampLsl
-            //    };
-            //}
+            EyeDataHandler.Instance.AddEyeDataPackage(ed);
         }
     }
 }
