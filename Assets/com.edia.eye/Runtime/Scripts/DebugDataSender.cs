5c4b8122ab593b9305373b4b43b9070053478c47
Jeroen De mooij <jeroen@the1stfloor.net>
2024-05-16 21:38:33 +0200

    MIsc

--- Assets/com.edia.eye/Runtime/Scripts/DebugDataSender.cs
+++ Assets/com.edia.eye/Runtime/Scripts/DebugDataSender.cs
@@ -1,62 +1,57 @@
-using Edia.Events;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Edia.Eye
-{
-    /// <summary>
-    /// Dummy class to fake a minimal data stream from an eye tracker (with 90Hz).
-    /// </summary>
-    public class DebugDataSender : MonoBehaviour
-    {
-        public bool IsRunning = false;
-        public static ILslTimer LslTimer;
-        public bool UseLslTiming = true;
-        double timestampLsl;
-        EyeDataPackage ed;
-        double randomWaitValue = 0.3f;
-        double lastTime;
+namespace Edia.Eye {
+	/// <summary>
+	/// Dummy class to fake a minimal data stream from an eye tracker (with 90Hz).
+	/// </summary>
+	public class DebugDataSender : MonoBehaviour {
+		public bool IsRunning = false;
+		public static ILslTimer LslTimer;
+		public bool UseLslTiming = true;
+		double timestampLsl;
+		EyeDataPackage ed;
+		double randomWaitValue = 0.3f;
+		double lastTime;
 
-        public void StartAddingDummyEyedata()
-        {
-            IsRunning = true;
-        }
+		public void StartAddingDummyEyedata() {
+			IsRunning = true;
+		}
 
-        // Sends random data to the eDIA `EyeDataHandler` 
-        void Update()
-        {
-            if (!IsRunning)
-                return;
+		// Sends random data to the eDIA `EyeDataHandler` 
+		void Update() {
+			if (!IsRunning)
+				return;
 
-            if (Time.time < (lastTime + randomWaitValue))
-                return;
+			if (Time.time < (lastTime + randomWaitValue))
+				return;
 
-            ed = new();
+			ed = new();
 
-            ed.eye = Edia.Constants.EyeId.CENTER.ToString().ToLower();
-            ed.position_x_local = 0f;
-            ed.position_y_local = 0f;
-            ed.position_z_local = 0f;
-            ed.diameter = UnityEngine.Random.Range(0.02f, 1.0f);
-            ed.rotation_x_local = UnityEngine.Random.Range(-15f, 15f);
-            ed.rotation_y_local = UnityEngine.Random.Range(-60f, 60f);
-            ed.rotation_z_local = 0f;
-            Quaternion eyeRot = Quaternion.Euler(ed.rotation_x_local, ed.rotation_y_local, 0);
-            Vector3 eyeFwd = eyeRot * Vector3.forward;
-            ed.direction_x_local = eyeFwd.x;
-            ed.direction_y_local = eyeFwd.y;
-            ed.direction_z_local = eyeFwd.z;
-            ed.timestamp_et = Time.realtimeSinceStartup;
+			ed.eye = Edia.Constants.EyeId.CENTER.ToString().ToLower();
+			ed.position_x_local = 0f;
+			ed.position_y_local = 0f;
+			ed.position_z_local = 0f;
+			ed.diameter = UnityEngine.Random.Range(0.02f, 1.0f);
+			ed.rotation_x_local = UnityEngine.Random.Range(-15f, 15f);
+			ed.rotation_y_local = UnityEngine.Random.Range(-60f, 60f);
+			ed.rotation_z_local = 0f;
+			Quaternion eyeRot = Quaternion.Euler(ed.rotation_x_local, ed.rotation_y_local, 0);
+			Vector3 eyeFwd = eyeRot * Vector3.forward;
+			ed.direction_x_local = eyeFwd.x;
+			ed.direction_y_local = eyeFwd.y;
+			ed.direction_z_local = eyeFwd.z;
+			ed.timestamp_et = Time.realtimeSinceStartup;
 
-            timestampLsl = LslTimer != null ? LslTimer.GetTime() : 0;
-            ed.timestamp_lsl = timestampLsl;
+			timestampLsl = LslTimer != null ? LslTimer.GetTime() : 0;
+			ed.timestamp_lsl = timestampLsl;
 
-            EyeDataHandler.Instance.AddEyeDataPackage(ed);
+			EyeDataHandler.Instance.AddEyeDataPackage(ed);
 
-            randomWaitValue = UnityEngine.Random.Range(0.01f, 1.0f);
-            lastTime = Time.time;
-        }
-    }
+			randomWaitValue = UnityEngine.Random.Range(0.01f, 1.0f);
+			lastTime = Time.time;
+		}
+	}
 }
