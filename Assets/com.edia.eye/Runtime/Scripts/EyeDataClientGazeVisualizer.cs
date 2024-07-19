c331cd3defe5fc36dc74c8e88b5dd9f5fc27b606
Felix Klotzsche <klotzsche@cbs.mpg.de>
2024-07-19 10:45:36 +0200

    change default of EyeDatapackage to zero; introd isValid flag

--- Assets/com.edia.eye/Runtime/Scripts/EyeDataClientGazeVisualizer.cs
+++ Assets/com.edia.eye/Runtime/Scripts/EyeDataClientGazeVisualizer.cs
@@ -25,6 +25,12 @@ namespace Edia.Eye
         Vector3 gazeOriginLocal;
         int counter = 0;
 
+        Color colorRay;
+        Color colorLeft = Color.green;
+        Color colorRight = Color.yellow;
+        Color colorCenter = Color.cyan;
+        Color colorInvalid = Color.red;
+
         private List<EyeDataPackage> receivedEyeDataSamples = new List<EyeDataPackage>();
 
 #endregion // -------------------------------------------------------------------------------------------------------------------------------
@@ -39,7 +45,8 @@ namespace Edia.Eye
             this.transform.localPosition = Vector3.zero;
             this.transform.localRotation = Quaternion.identity;
 
-			gazeRayRenderer.materials[0].color = Eye == Constants.EyeId.CENTER ? Color.cyan : Eye == Constants.EyeId.LEFT? Color.green: Color.yellow;
+            colorRay = Eye == Constants.EyeId.CENTER ? colorCenter : Eye == Constants.EyeId.LEFT ? colorLeft : colorRight;
+            gazeRayRenderer.materials[0].color = colorRay;
 
             counter = UpdateStep;
         }
@@ -47,13 +54,14 @@ namespace Edia.Eye
 #endregion // -------------------------------------------------------------------------------------------------------------------------------
 #region IEyeDataClient INTERFACE IMPLEMENTATION 
 
-		public void ProcessCurrentSamples (List<EyeDataPackage> currentSamples) {
-			receivedEyeDataSamples.Clear ();
-			foreach (var sample in currentSamples) {
-				if (sample.eye.ToLower() == Eye.ToString().ToLower())
-					receivedEyeDataSamples.Add (sample);
+		public void ProcessCurrentSamples(List<EyeDataPackage> currentSamples) {
+			receivedEyeDataSamples.Clear();
+            foreach (var sample in currentSamples) {
+                if (sample.eye.ToLower() == Eye.ToString().ToLower()) {
+                    receivedEyeDataSamples.Add(sample);
+                }
             }
-		}
+        }
 
 #endregion // -------------------------------------------------------------------------------------------------------------------------------
 #region PROCESSING SAMPLES
@@ -73,16 +81,33 @@ namespace Edia.Eye
             if (receivedEyeDataSamples.Count == 0)
                 return;
 
+            // Grab the first valid sample since last ray update:
+            int sampleIdx = -1;
+            for (int i = 0; i < receivedEyeDataSamples.Count; i++) {
+                if (receivedEyeDataSamples[i].isValid) {
+                    sampleIdx = i;
+                    break;
+                }
+            }
+
+            // for invalid samples, we skip:
+            if (sampleIdx < 0) {
+                gazeRayRenderer.materials[0].color = Color.red;
+                return;
+            } else {
+                gazeRayRenderer.materials[0].color = colorRay;
+            }
+
 			gazeOriginLocal = new Vector3(
-                receivedEyeDataSamples[0].position_x_local, 
-                receivedEyeDataSamples[0].position_y_local, 
-                receivedEyeDataSamples[0].position_z_local
+                receivedEyeDataSamples[sampleIdx].position_x_local, 
+                receivedEyeDataSamples[sampleIdx].position_y_local, 
+                receivedEyeDataSamples[sampleIdx].position_z_local
             );
 
 			gazeDirection = new Vector3(
-                receivedEyeDataSamples[0].direction_x_local, 
-                receivedEyeDataSamples[0].direction_y_local, 
-                receivedEyeDataSamples[0].direction_z_local
+                receivedEyeDataSamples[sampleIdx].direction_x_local, 
+                receivedEyeDataSamples[sampleIdx].direction_y_local, 
+                receivedEyeDataSamples[sampleIdx].direction_z_local
             );
 
             gazeRayRenderer.SetPosition(0, gazeOriginLocal + (Vector3.forward * gazeOriginOffsetZ));
--- Assets/com.edia.eye/Runtime/Scripts/EyeDataPackage.cs
+++ Assets/com.edia.eye/Runtime/Scripts/EyeDataPackage.cs
@@ -5,26 +5,29 @@
 namespace Edia.Eye
 {
     public class EyeDataPackage {
-        public string eye = "NA"; // Which eye the package is referring to (can be left, right, or center)
-        public float direction_x_local = float.NaN; // x element of normalized gaze vector in local space of HMD
-        public float direction_y_local = float.NaN; // y element of normalized gaze vector in local space of HMD
-        public float direction_z_local = float.NaN; // z element of normalized gaze vector in local space of HMD
-        public float position_x_local = float.NaN; // x coordinate of eye center vector in local space of HMD
-        public float position_y_local = float.NaN; // y coordinate of eye center vector in local space of HMD
-        public float position_z_local = float.NaN; // z coordinate of eye center vector in local space of HMD
-        public float rotation_x_local = float.NaN; // x rotation of the eye in local space of HMD
-        public float rotation_y_local = float.NaN; // y rotation of the eye in local space of HMD
-        public float rotation_z_local = float.NaN; // z rotation of the eye in local space of HMD
-        public float intersection_x = float.NaN; // x position of the intersection point with a plane in world space
-        public float intersection_y = float.NaN; // y position of the intersection point with a plane in world space
-        public float intersection_z = float.NaN; // z position of the intersection point with a plane in world space
-        public float diameter = float.NaN; // pupil diameter of the eye
-        public float diameter_x = float.NaN; // first axis of pupil ellipse
-        public float diameter_y = float.NaN; // second axis of pupil ellipse
-        public float confidence = float.NaN; // confidence in reliability of this sample (ideally in [0;1])
-        public float timestamp_et = float.NaN; // timestamp provided by the eye tracker
-        public double timestamp_lsl = float.NaN; // timestamp provided by Time.realtimeSinceStartup
-        public float openness = float.NaN; // openness value provided by SDK
-        public string target_id = "NA"; // identifier of the object that was intersected by the gaze ray
+        static float defaultValue = 0f;
+
+        public bool isValid = false;
+        public string eye = ""; // Which eye the package is referring to (can be left, right, or center)
+        public float direction_x_local = defaultValue; // x element of normalized gaze vector in local space of HMD
+        public float direction_y_local = defaultValue; // y element of normalized gaze vector in local space of HMD
+        public float direction_z_local = defaultValue; // z element of normalized gaze vector in local space of HMD
+        public float position_x_local = defaultValue; // x coordinate of eye center vector in local space of HMD
+        public float position_y_local = defaultValue; // y coordinate of eye center vector in local space of HMD
+        public float position_z_local = defaultValue; // z coordinate of eye center vector in local space of HMD
+        public float rotation_x_local = defaultValue; // x rotation of the eye in local space of HMD
+        public float rotation_y_local = defaultValue; // y rotation of the eye in local space of HMD
+        public float rotation_z_local = defaultValue; // z rotation of the eye in local space of HMD
+        public float intersection_x = defaultValue; // x position of the intersection point with a plane in world space
+        public float intersection_y = defaultValue; // y position of the intersection point with a plane in world space
+        public float intersection_z = defaultValue; // z position of the intersection point with a plane in world space
+        public float diameter = defaultValue; // pupil diameter of the eye
+        public float diameter_x = defaultValue; // first axis of pupil ellipse
+        public float diameter_y = defaultValue; // second axis of pupil ellipse
+        public float confidence = defaultValue; // confidence in reliability of this sample (ideally in [0;1])
+        public float timestamp_et = defaultValue; // timestamp provided by the eye tracker
+        public double timestamp_lsl = defaultValue; // timestamp provided by Time.realtimeSinceStartup
+        public float openness = defaultValue; // openness value provided by SDK
+        public string target_id = ""; // identifier of the object that was intersected by the gaze ray
     }
 }
