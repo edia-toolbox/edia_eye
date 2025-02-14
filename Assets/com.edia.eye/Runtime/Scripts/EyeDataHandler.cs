8c34f12d135f959d038bd272a3a86ea063209289
Felix Klotzsche <klotzsche@cbs.mpg.de>
2025-02-14 17:59:40 +0100

    fix DataHandler Lock

--- Assets/com.edia.eye/Runtime/Scripts/EyeDataHandler.cs
+++ Assets/com.edia.eye/Runtime/Scripts/EyeDataHandler.cs
@@ -1,87 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
+using Edia;
 using UnityEngine;
+using UXF;
 
 namespace Edia.Eye {
-    
+
+
     /// <summary>EyeData handler
     /// EyeDataHandler 
     /// - is a singleton
     /// - receives data from an EyeTracker (or something that pretends to be) via its public `AddEyeDataPackage()` method
-    /// - puts these samples into an internal list `currentSamples`
+    /// - puts these samples into an internal list `_currentSamples`
     /// - has a list of registered `DataClients` (Instances that implement `IEyeDataClient`) 
     /// - on the end of each frame
-    /// 	- activates the `Lock` to protect the access to `currentSamples` (to avoid new values being written by another thread while it's reading out)
-    /// 	- pushes the samples currently in the `currentSamples` to all DataClients via their `ProcessCurrentSamples()` method
-    /// 	- empties the `currentSamples`
+    /// 	- activates the `Lock` to protect the access to `_currentSamples` (to avoid new values being written by another thread while it's reading out)
+    /// 	- pushes the samples currently in the `_currentSamples` to all DataClients via their `ProcessCurrentSamples()` method
+    /// 	- empties the `_currentSamples`
     /// 	- releases the lock again
     /// </summary>
-    public sealed class EyeDataHandler : MonoBehaviour {
+    /// 
 
-        [Header("Refs")]
-        public List<MonoBehaviour> DataClients;
+    public sealed class EyeDataHandler : MonoBehaviour
 
-        [Header("Debug")]
-        public bool AutoStartProcessing = false;
+	{
+		public static EyeDataHandler Instance;
 
-        List<EyeDataPackage> _currentSamples { get; } = new List<EyeDataPackage>();
+		[Header("Refs")]
+		public List<MonoBehaviour> DataClients;
+
+		[Header("Debug")]
+		public bool ProcessOnStart = false;
 
         // Lock for thread safety:
-        private readonly object _lock = new object();
+        public readonly object Lock = new object();
 
-        // TODO: extend script from Edia.core <singeleton>  
-        // Pseudo-Singleton pattern to make sure we have only one DataHandler in the scene:
-        public static EyeDataHandler Instance;
-        private void Awake() {
-            if (Instance == null) {
-                Instance = this;
-            }
-            else if (Instance != this) {
-                Destroy(this.gameObject);
-            }
-        }
+        List<EyeDataPackage> _currentSamples { get; } = new List<EyeDataPackage> ();
 
-        private void Start() {
-            if (AutoStartProcessing)
-                StartPushingSamples();
-        }
+		// Pseudo-Singleton pattern to make sure we have only one DataHandler in the scene:
+		private void Awake () {
+			if (Instance == null) {
+				Instance = this;
+			} else if (Instance != this) {
+				Destroy (this.gameObject);
+			}
+		}
 
-        /// <summary> Start the coroutine that sends data to the `DataClients`. Called when recording should start </summary>
-        public void StartPushingSamples() {
-            Debug.Log("Started pushing");
-            ResetCurrentSamplesCollection();
-            StartCoroutine(PushSamplesThreadsafeAndReset());
-        }
+		private void Start() {
+			if(ProcessOnStart)
+				StartPushingSamples();
+		}
 
-        /// <summary> Coroutine that pushes the items in `currentSamples` to all `DataClients` in a somewhat threadsafe way. 
-        /// Empties the list after all samples are sent.  </summary>
-        private IEnumerator PushSamplesThreadsafeAndReset() {
-            while (true) {
-                yield return new WaitForEndOfFrame();
+		/// <summary>
+		/// Start the coroutine that sends data to the `DataClients`. Called when recording should start
+		/// </summary>
+		public void StartPushingSamples () {
+			Debug.Log ("Started pushing");
+			ResetCurrentSamplesCollection ();
+			StartCoroutine (PushSamplesThreadsafeAndReset ());
+		}
 
-                if (_currentSamples.Count == 0)
-                    continue;
+		/// <summary> Coroutine that pushes the items in `_currentSamples` to all `DataClients` in a somewhat threadsafe way. 
+		/// Empties the list after all samples are sent.  </summary>
+		private IEnumerator PushSamplesThreadsafeAndReset () {
+			while (true) {
+				yield return new WaitForEndOfFrame ();
 
-                lock (_lock) {
-                    // Push listed samples to all clients
-                    foreach (IEyeDataClient dataClient in DataClients) {
-                        dataClient.ProcessCurrentSamples(_currentSamples);
-                    }
+				if (_currentSamples.Count == 0)
+					continue;
 
-                    ResetCurrentSamplesCollection(); // Reset Queue
-                }
-            }
-        }
+				lock (Lock) {
+					// Push listed samples to all clients
+					foreach (IEyeDataClient dataClient in DataClients) {
+						dataClient.ProcessCurrentSamples (_currentSamples);
+					}
 
-        /// <summary>Empties the List of `currentSamples`. </summary>
-        private void ResetCurrentSamplesCollection() {
-            _currentSamples.Clear();
-        }
+                    ResetCurrentSamplesCollection (); // Reset Queue
+				}
+			}
+		}
 
-        /// <summary> Add samples to the list of `currentSamples`.  </summary>
-        /// <param name="latestSample">Latest sample recorded by the eye tracker.</param>
-        public void AddEyeDataPackage(EyeDataPackage latestSample) {
-            _currentSamples.Add(latestSample);
-        }
-    }
+		/// <summary>Empties the List of `_currentSamples`. </summary>
+		private void ResetCurrentSamplesCollection () {
+			_currentSamples.Clear ();
+		}
+
+		/// <summary> Add samples to the list of `_currentSamples`.  </summary>
+		/// <param name="latestSample">Latest sample recorded by the eye tracker.</param>
+		public void AddEyeDataPackage (EyeDataPackage latestSample) {
+            _currentSamples.Add (latestSample);
+		}
+	}
 }
