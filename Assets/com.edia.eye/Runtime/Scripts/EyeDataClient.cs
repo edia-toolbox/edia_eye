762e5fd57ab6931756f571c87d82b36d1e79e31d
eioe <klotzsche@cbs.mpg.de>
2025-04-25 15:28:03 +0200

    add EyeDataClient

---
+++ Assets/com.edia.eye/Runtime/Scripts/EyeDataClient.cs
@@ -0,0 +1,25 @@
+using Edia.Eye;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Edia.Eye {
+    public abstract class EyeDataClient : MonoBehaviour, IEyeDataClient
+    {
+        [Header("References")]
+        [Tooltip("Add link to Eye Data Handler Prefab in the scene.")]
+        public GameObject EyeDataHandler;
+
+        private void AddToEyeDataHandler()
+        {
+            // add to the list of data clients on the EyeDataHandler
+            EyeDataHandler.GetComponent<EyeDataHandler>().AddDataClient(this);
+        }
+
+        public abstract void ProcessCurrentSamples(List<EyeDataPackage> currentSamples);
+
+        private protected virtual void Awake()
+        {
+            AddToEyeDataHandler();
+        }
+    }
+}
