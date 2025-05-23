787c1a10247c61da0c7e538e977bfb2851181753
Jeroen De mooij <jeroen@the1stfloor.net>
2025-05-23 17:49:49 +0200

    Intersection check at latestsample

--- Assets/com.edia.eye/Runtime/Scripts/EyeDataClient.cs
+++ Assets/com.edia.eye/Runtime/Scripts/EyeDataClient.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 using System.Collections.Generic;
 
 namespace Edia.Eye {
-    public abstract class EyeDataClient : MonoBehaviour, IEyeDataClient
-    {
+    public abstract class EyeDataClient : MonoBehaviour, IEyeDataClient {
         [Header("References")]
         [InspectorHeader("EDIA EYE", "EyeData Client", "Eye data package client")]
-        [Tooltip("Add link to Eye Data Handler Prefab in the scene.")]
-        public GameObject EyeDataHandler;
+        public string dummyforheaderandwillberemoved;
 
         private void AddToEyeDataHandler()
         {
+            if (EyeDataHandler.Instance == null) {
+                Debug.LogError("EyeDataHandler is not in the scene. Please add it to the scene.");
+                return;
+            }
+            
             // add to the list of data clients on the EyeDataHandler
-            EyeDataHandler.GetComponent<EyeDataHandler>().AddDataClient(this);
+            EyeDataHandler.Instance.AddDataClient(this);
         }
 
         public abstract void ProcessCurrentSamples(List<EyeDataPackage> currentSamples);
